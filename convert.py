dc8cb7865b89fd3d8283f714ccce96f148ab2664
Serban Constantin <serban.constantin@gmail.com>
2012-03-26 20:04:19 +0100

    Added function to get the channel name from the log filename and tests, function to find the log date and tests and placeholder for the final processed log writing method.

--- convert.py
+++ convert.py
@@ -1,8 +1,47 @@
 import os
 from ConfigParser import SafeConfigParser
 
+def get_log_name(log):
+	logs                    = str(log)
+	fileName, fileExtension = os.path.splitext(logs) # get file name and extension
+	name_start = fileName.find('#')
+	name_end = fileName.find('.', name_start)
+	name = fileName[name_start:name_end]
+	
+	#tests
+	print fileName
+	print name
+
+	return name
+
+def find_date(log):
+	myfile = open(log)
+	myfile.seek(0)
+	log_text = myfile.read()
+	myfile.close()
+
+	log_day = log_text[:2]
+	log_month = log_text[3:5]
+	log_year = log_text[6:10]
+
+	#tests
+	print 'Day is ' + log_day
+	print 'Month is ' + log_month
+	print 'Year is ' + log_year
+
+	return log_day, log_month, log_year
+
+def write_log(day, month, year):
+	to_write = open(output + logs + '.html', 'w')
+	to_write.write(log_html)
+	to_write.close()
+
 # read config file
 parser = SafeConfigParser()
 parser.read('setup.cfg')
 log    = parser.get('directories', 'input')
 output = parser.get('directories', 'output')
+
+day, month, year = find_date(log)
+
+get_log_name(log)
